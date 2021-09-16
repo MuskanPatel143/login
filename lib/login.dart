1ac986f628c4c3c26f83bb97352d49d932b97ec9
muskan-patel <muskanpatel10919.99@gmail.com>
2021-09-16 11:00:22 +0530

    mussi

---
+++ lib/login.dart
@@ -0,0 +1,309 @@
+import 'package:design1/page1.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late String email, password;
+  Widget _buildLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 70),
+          child: Text(
+            'Hello!',
+            style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height / 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildEmailRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isNotEmpty) {
+            return 'required';
+          } else {
+            return null;
+          }
+        },
+        onChanged: (value) {
+          setState(() {
+            email = value;
+          });
+        },
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.email,
+            color: mainColor,
+          ),
+          labelText: ('E-mail'),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPasswordRow() {
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: TextFormField(
+        keyboardType: TextInputType.text,
+        obscureText: true,
+        onChanged: (value) {
+          setState(() {
+            password = value;
+          });
+        },
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.lock,
+            color: mainColor,
+          ),
+          labelText: ('Password'),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForgetPasswordButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        TextButton(
+          onPressed: () {},
+          child: Text('Forget Password'),
+        ),
+      ],
+    );
+  }
+
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  void validate() {
+    if (formkey.currentState!.validate()) {
+      print("validated");
+    } else {
+      print("Not Validated");
+    }
+  }
+  // validateEmail(String value) {
+  //   RegExp regex = new RegExp(
+  //       r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+  //       r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+  //       r"{0,253}[a-zA-Z0-9])?)*$");
+  //   // ignore: unnecessary_null_comparison
+  //   if (!regex.hasMatch(value) || value == null)
+  //     return 'Enter a valid email address';
+  //   else
+  //     return null;
+  // }
+
+  Widget _buildLoginButton() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+      Container(
+        color: mainColor,
+        height: MediaQuery.of(context).size.height / 20,
+        width: MediaQuery.of(context).size.width / 5,
+        margin: EdgeInsets.only(bottom: 20),
+        child: TextButton(
+          onPressed: () {
+            validate();
+            // validateEmail(email);
+
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(builder: (context) => MyHomePage()),
+            // );
+          },
+          child: Text('Login'),
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+        ),
+      )
+    ]);
+  }
+
+  Widget _buildOrRow() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: Form(
+        key: formkey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Divider(
+                color: Colors.blueGrey,
+                height: 1.5,
+              ),
+            ),
+            Text(
+              '- OR -',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Expanded(
+              child: Divider(
+                color: Colors.blueGrey,
+                height: 1.5,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: mainColor,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    offset: Offset(0, 3),
+                    blurRadius: 6.0)
+              ],
+            ),
+            child: Icon(
+              FontAwesomeIcons.google,
+              color: Colors.white,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildContainer() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height / 30,
+                      ),
+                    )
+                  ],
+                ),
+                _buildEmailRow(),
+                _buildPasswordRow(),
+                _buildForgetPasswordButton(),
+                _buildLoginButton(),
+                _buildOrRow(),
+                _buildSocialButton(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildSignUpBtn() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(top: 40),
+          child: TextButton(
+            onPressed: () {},
+            child: RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                  text: "Don't have an account? ",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: MediaQuery.of(context).size.height / 40,
+                      fontWeight: FontWeight.w400)),
+              TextSpan(
+                  text: "Sign Up",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: MediaQuery.of(context).size.height / 40,
+                      fontWeight: FontWeight.bold))
+            ])),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.blueGrey,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: const Radius.circular(70),
+                      bottomRight: const Radius.circular(70))),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _buildLogo(),
+              _buildContainer(),
+              _buildSignUpBtn(),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
